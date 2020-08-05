5f95fa35a26f7e75f210f7253121681ee35615ad
Erik Zscheile <zseri.devel@ytrizja.de>
2020-08-05 17:29:34 +0200

    fix #1: call wake in Delay::poll

--- src/delay.rs
+++ src/delay.rs
@@ -11,12 +11,13 @@ pub struct Delay {
 
 impl Future for Delay {
     type Output = ();
-    fn poll(self: Pin<&mut Self>, _ctx: &mut Context) -> Poll<()> {
+    fn poll(self: Pin<&mut Self>, ctx: &mut Context) -> Poll<()> {
         let mut this = Pin::into_inner(self);
         if this.done {
             Poll::Ready(())
         } else {
             this.done = true;
+            ctx.waker().wake_by_ref();
             Poll::Pending
         }
     }
