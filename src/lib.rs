a37abe488568f7ec766ba264e036be2c860beb5f
James Laver <james.laver@gmail.com>
2020-08-05 10:00:18 +0200

    replace silly arc checking with proper closed status and drop handler

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ use ping_pong_cell::PingPongCell;
 
 pub fn oneshot<T: Send>() -> (Sender<T>, Receiver<T>) {
     let inner = Arc::new(Inner { cell: PingPongCell::new(None) });
-    let sender = Sender { inner: inner.clone() };
-    let receiver = Receiver { inner };
+    let sender = Sender { inner: Some(inner.clone()) };
+    let receiver = Receiver { inner: Some(inner) };
     (sender, receiver)
 }
 
@@ -19,56 +19,131 @@ pub enum WaitError {
     AlreadyAwaited(Waker),
 }
 
-pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+pub struct Sender<T: Send> {
+    inner: Option<Arc<Inner<T>>>,
 }
 
 // Can be polled as a Future to wait for a receiver to be listening.
 impl<T: Send> Sender<T> {
-    pub async fn send(self, value: T) -> Result<(), Closed> {
-        if Arc::strong_count(&self.inner) == 1 { return Err(Closed()); }
-        let ret = self.inner.cell.transact(|state| {
-            if let Some(State::Waker(waker)) = state.take() {
-                *state = Some(State::Ready(value));
-                Some(waker)
-            } else {
-                *state = Some(State::Ready(value));
-                None
+
+    /// Closes the channel by causing an immediate drop
+    pub fn close(self) { }
+
+    /// Sends a message on the channel
+    pub fn send(self, value: T) -> Result<(), Closed> {
+        if let Some(ref inner) = &self.inner {
+            let ret = inner.cell.transact(|state| {
+                match state.take() {
+                    Some(State::Closed) => Err(Closed()),
+                    Some(State::Waker(waker)) => {
+                        *state = Some(State::Ready(value));
+                        Ok(Some(waker))
+                    }
+                    _ => {
+                        *state = Some(State::Ready(value));
+                        Ok(None)
+                    }
+                }
+            });
+            match ret {
+                Ok(Some(waker)) => {
+                    waker.wake();
+                    Ok(())
+                }
+                Ok(None) => Ok(()),
+                Err(e) => Err(e),
             }
-        });
-        if let Some(waker) = ret {
-            waker.wake();
+        } else {
+            Err(Closed()) // not sure how you got here tbh
         }
-        Ok(())
     }
 }
 
-pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+pub struct Receiver<T: Send> {
+    inner: Option<Arc<Inner<T>>>,
+}
+
+impl<T: Send> Receiver<T> {
+
+    /// Closes the channel by causing an immediate drop
+    pub fn close(self) { }
+
 }
 
 enum State<T> {
     Waker(Waker),
     Ready(T),
+    Closed,
 }
 
 struct Inner<T> {
     cell: PingPongCell<State<T>>,
 }
 
-impl<T: Send> Future for Sender<T> {
-    type Output = ();
-    fn poll(self: Pin<&mut Self>, context: &mut Context) -> Poll<()> {
-        let this = Pin::into_inner(self);
-        this.inner.cell.transact(|state| {
-            if let Some(State::Waker(waker)) = state {
-                waker.wake_by_ref();
-                Poll::Ready(())
-            } else {
-                *state = Some(State::Waker(context.waker().clone()));
-                Poll::Pending
+impl<T: Send> Drop for Sender<T> {
+    fn drop(&mut self) {
+        if let Some(inner) = &self.inner {
+            let ret = inner.cell.transact(|state| {
+                match state.take() {
+                    Some(State::Waker(waker)) => {
+                        *state = Some(State::Closed);
+                        Some(waker)
+                    }
+                    // Could be Ready or Closed, either is fine
+                    Some(other) => {
+                        *state = Some(other);
+                        None
+                    }
+                    None => {
+                        *state = Some(State::Closed);
+                        None
+                    }
+                }
+            });
+            if let Some(waker) = ret {
+                waker.wake();
             }
-        })
+        }
+    }
+}
+
+impl<T: Send> Drop for Receiver<T> {
+    fn drop(&mut self) {
+        if let Some(inner) = &self.inner {
+            let ret = inner.cell.transact(|state| {
+                if let Some(State::Waker(waker)) = state.take() {
+                    *state = Some(State::Closed);
+                    Some(waker)
+                } else {
+                    *state = Some(State::Closed);
+                    None
+                }
+            });
+            if let Some(waker) = ret {
+                waker.wake();
+            }
+        }
+    }
+}
+
+impl<T: Send> Future for Sender<T> {
+    type Output = Result<Waker, Closed>;
+    fn poll(self: Pin<&mut Self>, context: &mut Context) -> Poll<Self::Output> {
+        let this = Pin::into_inner(self);
+        if let Some(inner) = &this.inner {
+            inner.cell.transact(|state| {
+                match state.take() {
+                    Some(State::Waker(waker)) => Poll::Ready(Ok(waker)),
+                    Some(State::Closed) => Poll::Ready(Err(Closed())),
+                    _ => {
+                        *state = Some(State::Waker(context.waker().clone()));
+                        Poll::Pending
+                    }
+                }
+            })
+        } else {
+            Poll::Ready(Err(Closed()))
+        }
     }
 }
 
@@ -76,24 +151,25 @@ impl<T: Send> Future for Receiver<T> {
     type Output = Result<T, Closed>;
     fn poll(self: Pin<&mut Self>, context: &mut Context) -> Poll<Result<T, Closed>> {
         let this = Pin::into_inner(self);
-        this.inner.cell.transact(|state| {
-            match state.take() {
-                Some(State::Ready(value)) => Poll::Ready(Ok(value)),
-                Some(State::Waker(waker)) => {
-                    *state = Some(State::Waker(context.waker().clone()));
-                    waker.wake();
-                    Poll::Pending
-                }
-                None => {
-                    if Arc::strong_count(&this.inner) == 1 {
-                        Poll::Ready(Err(Closed()))
-                    } else {
+        if let Some(inner) = &this.inner {
+            inner.cell.transact(|state| {
+                match state.take() {
+                    Some(State::Closed) => Poll::Ready(Err(Closed())),
+                    Some(State::Ready(value)) => Poll::Ready(Ok(value)),
+                    Some(State::Waker(waker)) => {
+                        *state = Some(State::Waker(context.waker().clone()));
+                        waker.wake();
+                        Poll::Pending
+                    }
+                    None => {
                         *state = Some(State::Waker(context.waker().clone()));
                         Poll::Pending
                     }
                 }
-            }
-        })
+            })
+        } else {
+            Poll::Ready(Err(Closed()))
+        }
     }
 }
 
