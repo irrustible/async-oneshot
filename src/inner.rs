6b9841fba02eadc09991a0598a9859dd8081080c
Erik Zscheile <zseri.devel@ytrizja.de>
2020-10-01 20:04:38 +0200

    Inner: add debug assertions

--- src/inner.rs
+++ src/inner.rs
@@ -36,11 +36,13 @@ impl<T> Inner<T> {
     // Gets the receiver's waker. You *must* check the state to ensure
     // it is set. This would be unsafe if it were public.
     pub fn recv(&self) -> &Waker { // MUST BE SET
+        debug_assert!(self.state().recv());
         unsafe { &*(*self.recv.get()).as_ptr() }
     }
 
     // Sets the receiver's waker.
     pub fn set_recv(&self, waker: Waker) -> State {
+        debug_assert!(!self.state().recv());
         let recv = self.recv.get();
         unsafe { (*recv).as_mut_ptr().write(waker) } // !
         State(self.state.fetch_or(RECV, AcqRel))
@@ -49,21 +51,25 @@ impl<T> Inner<T> {
     // Gets the sender's waker. You *must* check the state to ensure
     // it is set. This would be unsafe if it were public.
     pub fn send(&self) -> &Waker {
+        debug_assert!(self.state().send());
         unsafe { &*(*self.send.get()).as_ptr() }
     }
 
     // Sets the sender's waker.
     pub fn set_send(&self, waker: Waker) -> State {
+        debug_assert!(!self.state().send());
         let send = self.send.get();
         unsafe { (*send).as_mut_ptr().write(waker) } // !
         State(self.state.fetch_or(SEND, AcqRel))
     }
 
     pub fn take_value(&self) -> T { // MUST BE SET
+        debug_assert!(self.state().ready());
         unsafe { (*self.value.get()).as_ptr().read() }
     }
 
     pub fn set_value(&self, value: T) -> State {
+        debug_assert!(!self.state().ready());
         let val = self.value.get();
         unsafe { (*val).as_mut_ptr().write(value) }
         State(self.state.fetch_or(READY, AcqRel))
